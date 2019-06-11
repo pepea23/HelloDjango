67ec85fe9d2080eaad0e2ec09d20cf7f8cf2dbbc
root <root@instance-1.c.testdeploy-243310.internal>
2019-06-11 08:49:42 +0000

    Eidt admin config in polls

--- helloworld/polls/admin.py
+++ helloworld/polls/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
+
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInLine):
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-            (None,{'fields':['question_text']}),
-            ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
-            ]
-            inlines = [ ChoiceInline ]
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
-
-admin.site.register(Question,QuestionAdmin)
-# Register your models here.
+admin.site.register(Question, QuestionAdmin)
