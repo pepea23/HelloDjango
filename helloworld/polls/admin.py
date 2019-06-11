87802497574a6afead32ba3a75f2cde43aaa6d7d
root <root@instance-1.c.testdeploy-243310.internal>
2019-06-11 04:53:48 +0000

    Edit polls/admin

--- helloworld/polls/admin.py
+++ helloworld/polls/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-admin.site.register(Choice)
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInLine):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None,{'fields':['question_text']}),
+            ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+            ]
+            inlines = [ ChoiceInline ]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question,QuestionAdmin)
 # Register your models here.
