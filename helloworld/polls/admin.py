832a79b3301986b127c42b2619707e18b9627263
Ittidate <pepea.23@live.com>
2019-06-11 17:26:09 +0700

    :recycle: codestyle

--- helloworld/polls/admin.py
+++ helloworld/polls/admin.py
@@ -10,11 +10,15 @@ class ChoiceInline(admin.StackedInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        (None, {'fields': ['question_text']}),
+        ('Date information', {
+            'fields': ['pub_date'], 
+            'classes': ['collapse']
+        }),
     ]
     inlines = [ChoiceInline]
     list_filter = ['pub_date']
     search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
