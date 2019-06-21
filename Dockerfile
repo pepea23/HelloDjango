9aae427fe2ea6bc8f849751d4a0b9a13ec058305
root <pepea.23@live.com>
2019-06-21 04:57:35 +0000

    Error Docker run pls fix

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 From python:3.7
-COPY . /app/
 WORKDIR /app/
+COPY . /app/
+EXPOSE 8000
 run pip install -r requirements.txt
 
-ENTRYPOINT ["sh","-c", "cd helloworld && python manage.py migrate && python manage.py runserver 0.0.0.0:8000"]
+ENTRYPOINT ["uwsgi", "--ini", "hellodocker.ini"]
+#ENTRYPOINT ["sh","-c", "cd helloworld && python manage.py migrate && python manage.py runserver 0.0.0.0:8000"]
--- helloworld/helloworld/settings.py
+++ helloworld/helloworld/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 DATABASES = {
         'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+       # 'USER': 'postgres',
+       # 'HOST': 'db',
+       # 'PORT': 5432,
     }
 }
 
